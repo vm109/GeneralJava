1e9dca19a2b975a532e57d9e23cccc90fe29d403
venkata mangina <venkatamangina@gmail.com>
2017-01-21 21:52:37 -0500

    Commited with The constructor return and constructor final

---
+++ GeneralJava/src/constructors/ConstructorReturn.java
@@ -0,0 +1,18 @@
+package constructors;
+
+public class ConstructorReturn {
+//
+//	public ConstructorReturn() {
+//		// it is returning an compilation error.
+//	return 1;
+//	}
+	
+//	public final ConstructorReturn(){
+//		
+//	}
+	
+	public final int a;
+	public ConstructorReturn() {
+	a = 2;
+	}
+}
